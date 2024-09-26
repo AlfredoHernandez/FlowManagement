a7dc58872b6166b4e938f24af6f25518a8b4650c
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2024-09-25 18:54:11 -0600

    Initial Commit

---
+++ FlowExample/FlowExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowExampleApp.swift
+//  FlowExample
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
